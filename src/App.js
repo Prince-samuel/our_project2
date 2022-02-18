48f359ee5eab73abf254a64c65e909fb90bf4155
Prince-samuel <95848427+Prince-samuel@users.noreply.github.com>
2022-02-18 10:01:45 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Body from './components/Body/Body';
+import './App.css';
+import './components/Body/Body.css';
+import './components/Left/Left.css';
+import './components/Right/Right.css';
+import './components/Navbar/Navbar.css';
+import './components/Searchbar/Searchbar.css';
+
+
+
+function App() {
+  return (
+    <div className="website">
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
